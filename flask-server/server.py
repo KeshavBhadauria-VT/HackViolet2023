ba70b379e837795d592992648b1f24417f03754c
KeshavBhadauria-VT <82663609+KeshavBhadauria-VT@users.noreply.github.com>
2023-02-08 12:51:59 -0500

    Update server.py
    firebase being annoying

--- flask-server/server.py
+++ flask-server/server.py
@@ -17,16 +17,7 @@ app = Flask(__name__)
 
 
 cred = credentials.Certificate({
-  "type": "service_account",
-  "project_id": "hackviolet-2023",
-  "private_key_id": "0d95e768301f788203360a3c314d1ff4b49f210f",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC/11gCF3viRm8w\n1Iw0ylabLaWDjPcEc6A0I+wGHeZ3yP6BtEavcLRxtyZA1LOhNY+IT7KQdWwnn4lS\nsqWJr1fQsrPMg9L1+99hQ0UvJawNVA5Ya4nBA5TO+n8VjQXKYnFbo85LxjYyrkVp\nu26RGToMXiJEeUN4u6BRYMzT49ERk7lRD2ytq8J/RAoQEGtqI2nrFw3vt8fP8etE\nh8+lqQrHPWKMUlYO1m93ZmpDlNfFq71gULXxKAfyxjD6d832PVO9rqVcj9VyN6mL\nYxcrzoLs504LP1GkJigaELXnP+MSo1KSLQ87GVnjRazuy6rqAi/iny6Koqr1YsM+\nFHDRwsPjAgMBAAECggEAMT7+9R6BKz9oeLD/Quf0Hoazt9bebFKgO/JCuvk++d3r\nU3FrOcslTxG0bK0NZaNBsry+um2iM3O1RTjd3L/Zu5W8KRJn+8RJ/gZOQsGZbpx2\nyETmylj4gL92Exqt81uc/OjKYtJBhHYPFIrMYcwHhjg99LTqWJpVl75RJk2TlD6V\nUsMRXkgyF8PkDKcbBf1SjUrDzhsRiwWlVh63mKu23uoG2uKab9ndz4+WP4nGVMUq\nH27wgnfn3Lu97HqdOufLhjgFgk/zWG6DJ4go//Erkilp01Za6X8ye+Z+p55zhaUF\nH6PNVmsNAPfgB7nJAwriKqvQqgqJCybhUqoxEpt5YQKBgQDm4eS8lGLPHLMx1mZ9\n3rd4CjAwAvWw8OO3zv7yVozneNZoHGC/V0p09r+SNlP0rIFsjykjYbm0ekmRMM2p\ndP3i0tjzAVqTmnciearMwUaWaeb3A82yZEwNOIGzYon663zJYOXOnwPMzHq1XZMk\ncsaWVDNVZhJeTdNhLH/MJl+MoQKBgQDUtiWv6x+xNx8/EUDQq1VqbKIKL1Vom2YO\nsukrG06iX01VdzACw779jAkUN5YWh8qkQcQAjm+AFY+NLo0ujDQbCoICJe1PSeNN\n6Bbqeu6dQ9/n0C5l5FQsrurbj8d49lWY8IBiXWZ1yMFZnYDqZayYv78xdK1Y1yEG\nB3QhAGFeAwKBgChJysxNaGCTtYFA1Gmqb4F25am7GnxhzJrKeBp5l+TcZS2NpiQ9\nazS1ygANkuDjKUJ0SM1vxCQGIsg/J+QmEhd+i3T8C34uOhO9/1vzbgnZOQBpnX8/\nmTXFYnyW+VTbus65JeDERrl6BP+Y8JAvpw+cacjBIvtpwTT3uuU43PuBAoGBAIUW\ntnvcZEbwU0eVlvU4zke5UqWGuiyqVm/J92P2QPwNHwjjexGvwhdIAAI7SVgtfFfL\nokQq2o7MbrYKM2G1bYCh0XhgnJuogmUjWvs5EYUqi7SyJ+WuuBABHYdk7Yy8rA/2\nIo3pHIuDoZ5dkuSV2yjziD/8Xd2hNNNd5IYlg8ZbAoGAUVobw1cII2zx7KbFiFDC\nIPzu5OgHO2bih/B1OEGUkOPdNv5ulUyG+HOJlrrHrVhGVRsnpjGQh5enNG6IGJcm\n91q0zbHH45QZ6oCuwTacGomhzV9/DZY4w2GBcyfAUegn4hP+2IrXTSQ262OKLs+E\nqXGSvoZpeML1YESnxpzpqqo=\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-lezg2@hackviolet-2023.iam.gserviceaccount.com",
-  "client_id": "112982398775381512907",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-lezg2%40hackviolet-2023.iam.gserviceaccount.com"
+
 })
 
 
